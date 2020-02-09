4380d049506cf79caadefa172f2a8b15ef91c34c
ZheleznyakovD <49455501+Megapixary@users.noreply.github.com>
2020-02-09 14:38:38 +0300

    Сделал адаптив

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
-    nextEl: '.arrow-next',
+    nextEl: '.arrow',
   },
+  breakpoints: {
+    // when window width is >= 480px
+    475: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    }
+  }
+});
+
+var menuButton = document.querySelector('.burger');
+var menu = document.querySelector('.nav-bar');
+
+menuButton.addEventListener('click', function() {
+  menu.classList.toggle('nav-bar-active');
+  menuButton.classList.toggle('burger-active');
 });
