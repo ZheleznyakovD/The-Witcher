e1778467c027fb18490da5884ef2c1c7142d6c65
ZheleznyakovD <49455501+Megapixary@users.noreply.github.com>
2020-02-11 20:23:23 +0300

    сверстал Описание и Отзывы и адаптировал

--- js/main.js
+++ js/main.js
@@ -13,6 +13,25 @@ var mySwiper = new Swiper('.swiper-container', {
   }
 });
 
+var feedbackSwiper = new Swiper('.swiper-feedback', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: '.bowandarrow',
+  },
+  breakpoints: {
+    // when window width is >= 480px
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 45
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 45
+    }
+  }
+});
+
 var menuButton = document.querySelector('.burger');
 var menu = document.querySelector('.nav-bar');
 
